9be06eae8020138e8ec3270e8c9e081dbf28819e
Nolan Flatt <Nolan@iMac-27-inch.local>
2015-08-15 12:36:07 -0700

    Initial Commit

---
+++ Project 8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 8
+//
+//  Created by Nolan Flatt on 8/15/15.
+//  Copyright (c) 2015 Nolan Flatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
